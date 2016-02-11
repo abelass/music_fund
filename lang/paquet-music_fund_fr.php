71238aa908cce6c8068ac23e543ea5052cbc6012
abelass <rainer@websimple.be>
2016-02-11 16:22:32 +0100

    import initial

---
+++ lang/paquet-music_fund_fr.php
@@ -0,0 +1,14 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'music_fund_description' => '',
+	'music_fund_nom' => 'Music Fund',
+	'music_fund_slogan' => '',
+);
+
+?>
