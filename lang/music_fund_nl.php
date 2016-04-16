1afef94722c8c877c196081aa5a5f0b66ffa64da
Rainer <rainer@websimple.be>
2016-04-16 11:01:29 +0200

    ne pas écrire des textes en dur, utiliser les chaines de langues

--- lang/music_fund_nl.php
+++ lang/music_fund_nl.php
@@ -6,7 +6,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 		
 	// G
-	'get_in_touch' => 'N\'hésitez pas à prendre contact',
+	'get_in_touch' => 'Contacteer ons',
 
 	// M
 	'music_fund_titre' => 'Music Fund',
