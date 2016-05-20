82331cff48a6732e20a112246f2079dd890d3874
abelass <rainer@websimple.be>
2016-05-20 16:30:10 +0200

    édition

--- lang/music_fund_fr.php
+++ lang/music_fund_fr.php
@@ -6,9 +6,15 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 	//C
 	'creer_article' => 'Créer un article...',
-	//D
+	'creer_contact' => 'Créer un contact..',
+		
+	// D
 	'donation_taxe' => 'Pour faire un don avec déduction fiscale, voir ci-après.<br />Merci pour votre aide!',
 	'donate' => 'Faire un don',
+		
+	// E
+	'editer_article' => 'Edition article',
+	'editer_contact' => 'Edition contact',
 
 	//F
 	'footer_title' => 'Sous le Haut Patronage de Sa Majesté la Reine Mathilde',
