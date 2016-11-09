f0af42fe3ccd39eb2794e084c5d47bc5c7c85337
abelass <rainer@websimple.be>
2016-11-09 16:21:27 +0100

    désactiver l'héritage des logos

---
+++ music_fund_options.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Fichier gérant les options du plugin
+ *
+ * @plugin     Music Fund
+ * @copyright  2016
+ * @author     Rainer Müller
+ * @licence    GNU/GPL
+ * @package    SPIP\Music_fund\Installation
+ */
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+define('_LOGO_RUBRIQUE_DESACTIVER_HERITAGE', true);
